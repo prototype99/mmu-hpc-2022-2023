454aa49364d6468c0c633b3d3a4b2044b26b23a2
seirra blake <sophietheopossum@yandex.ru>
2023-04-10 20:41:39 +0100

    l o o p

--- build.sh
+++ build.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
-#could this be simplified? probably, but let's just play it safe
-FLAG=0
+for FLAG in 0 1 2 3 fast
+do
 cc ASSESS-barrier.c -O${FLAG} -o ASSESS-barrier-O${FLAG}.exe
 mv ASSESS-barrier-O${FLAG}.exe $WORK/a
-FLAG=1
-cc ASSESS-barrier.c -O${FLAG} -o ASSESS-barrier-O${FLAG}.exe
-mv ASSESS-barrier-O${FLAG}.exe $WORK/a
-FLAG=2
-cc ASSESS-barrier.c -O${FLAG} -o ASSESS-barrier-O${FLAG}.exe
-mv ASSESS-barrier-O${FLAG}.exe $WORK/a
-FLAG=3
-cc ASSESS-barrier.c -O${FLAG} -o ASSESS-barrier-O${FLAG}.exe
-mv ASSESS-barrier-O${FLAG}.exe $WORK/a
-FLAG=fast
-cc ASSESS-barrier.c -O${FLAG} -o ASSESS-barrier-O${FLAG}.exe
-mv ASSESS-barrier-O${FLAG}.exe $WORK/a
+done
