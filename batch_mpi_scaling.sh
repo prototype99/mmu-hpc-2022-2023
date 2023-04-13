efece7c888f548ad0d7249bb77de19cf3a4e9dcd
seirra blake <sophietheopossum@yandex.ru>
2023-04-13 13:46:14 +0100

    step in 16

--- batch_mpi_scaling.sh
+++ batch_mpi_scaling.sh
@@ -49,7 +49,7 @@ TMPFILE=/tmp/${USER}-mpi.txt
 # loop over various number of MPI processes, copying timing output to $TIMING
 for NODES in 1 2 3 4
 do
-for PROCS in 8 16 24 32 40 48 56 64 72 80 88 96 104 112 120 128
+for PROCS in 16 32 48 64 80 96 112 128
 do
 echo Running $EXE in directory $PWD on $PROCS MPI processes
 srun --nodes=${NODES} --ntasks=${PROCS} --cpu-bind=verbose,rank ./${EXE} | tee ${TMPFILE}
